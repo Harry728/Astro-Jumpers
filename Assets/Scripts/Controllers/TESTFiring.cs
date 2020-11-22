101fd6770052d795d6c9632bcb909af4a92636e2
abapandy <abapandy@gmail.com>
2020-11-21 23:29:36 -0500

    DAD - Bullet, firing

---
+++ Assets/Scripts/Controllers/TESTFiring.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TESTFiring : MonoBehaviour
+{
+    [SerializeField] GameObject projectile = null;
+    [SerializeField] Transform spawnPoint;
+    [SerializeField] float force = 50;
+
+    void FireLeft()
+    {
+        GameObject bullet = Instantiate(projectile, spawnPoint.position, projectile.transform.rotation, null);
+        bullet.GetComponent<Rigidbody>().AddForce(Vector3.forward * force, ForceMode.Impulse);
+    }
+}
